32a6b87bb2a1717094540144bc2f32bf49946732
Sarah Carlson <sbcarls@gmail.com>
2020-12-03 23:32:46 -0500

    updated code

---
+++ routes/api.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const fs = require("fs");
+const dbPath = path.join(__dirname, "../db/db.json");
+let notes = [];
+var { v4: uuidv4 } = require("uuid");
+
+module.exports = function(app) {
+    // GET request
+    app.get("/api/notes", function(req, res) {
+      fs.readFile(dbPath, "utf8", function(err, data) {
+          if (err){
+              console.log(err);
+          } else {
+              notes = JSON.parse(data);
+              res.json(notes);
+          }
+      });
+    });
+
+    // POST request
+    app.post("/api/notes", (req, res) => {
+        let newNote = req.body;
+        let id = uuidv4();
+        newNote.id = id;        
+        fs.readFile(dbPath, "utf8", (err, data) => {
+            if (err) {
+                console.log(err)
+            }else{
+                notesArray = JSON.parse(data);
+                notesArray.push(newNote);
+                const newObj = JSON.stringify(arrayNotes);
+                fs.writeFile(dbPath, newObj, err => err ? console.log(err) : console.log("Notes Saved"));
+            }            
+        })
+        res.json(newNote);        
+    });
+
+    // DELETE request
+    app.delete("/api/notes/:id", function(req, res) {
+        const id = req.params.id;
+        fs.readFile(dbPath, "utf8", function(error, data) {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                arrayNotes = JSON.parse(data);
+                const updatedNotes = arrayNotes.filter(notes => notes.id !== id);
+                const updatedNotesObj = JSON.stringify(updatedNotes);
+                // Write db.json file with the stringify updateNotes.
+                fs.writeFile(dbPath, updatedNotes, function() {
+                  if (err) {
+                    console.log(error)
+                  } 
+                  else {
+                    console.log("Updated");
+                  }
+                  return res.json(true);
+                }
+              )}
+          })
+      });
+}
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 
-// Set up initial port
+// Initializes port
 const PORT = process.env.PORT || 3001;
 
+// Express
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname +"public")));
+
+app.use(express.json());
+
+// File Mapping
+require("./routes/api")(app);
+require("./routes/html")(app);
+
+// Start server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
